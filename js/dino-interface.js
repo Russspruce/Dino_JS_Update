ece1e3ad97b35dfe2d57099f17559337284effa5
Andrew Russo and Jeffrey Hackford <andrew@>
2016-05-26 14:33:52 -0700

    Working dino game with complete code and correct indentation.  Need to add styling and flush out spare testing code.

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -1,79 +1,42 @@
 var apiKey = require('./../.env').apiKey;
 var Dino = require('./../js/dino.js').DinoModule;
+var fakeDinosArray = require('./../js/dino.js');
+var dinoArray2 = [];
+
 
 var displayDino = function(dinoArray) {
   var shuffleDino = new Dino();
-  shuffleDino.shuffle(dinoArray);
-  var dinoChoices = [];
+  dinoArray2.push(dinoArray[0]);
+  dinoArray2.push(dinoArray[1]);
+  dinoArray2.push(dinoArray[2]);
 
-  // console.log(dinoArray);
-  // dinoChoices.push(dinoArray[0]);
-  // dinoChoices.push(dinoArray[1]);
-  // dinoChoices.push(dinoArray[2]);
-  // console.log(dinoChoices);
-  $("#dino1random").text(dinoArray[0]);
-  $("#dino2random").text(dinoArray[1]);
-  $("#dino3random").text(dinoArray[2]);
+  shuffleDino.shuffle(dinoArray);
+
+  $('#dino1random').html("<h2 value='dino1random'>" + dinoArray[0] + "</h2>");
+  $("#dino2random").html("<h2 value='dino2random'>" + dinoArray[1] + "</h2>");
+  $("#dino3random").html("<h2 value='dino3random'>" + dinoArray[2] + "</h2>");
   dinoArray = [];
 };
 
 $(document).ready(function(){
-var dinoTest = new Dino();
+  var dinoTest = new Dino();
 
   $('#dino').submit(function(event) {
     event.preventDefault();
-      dinoTest.getDino(displayDino);
+    dinoTest.getDino(displayDino);
+  });
+
+
+  $("#user_form").submit(function(event) {
+    event.preventDefault();
+    debugger;
+    var userGuess = $('#user_input').val();
+    if (userGuess === dinoArray2[2]) {
+      alert("you win!");
+    } else if (userGuess === dinoArray2[0] || userGuess === dinoArray2[1]){
+      alert("you lose");
+    } else {
+      alert("hi");
+    }
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var apiKey = require('./../.env').apiKey;
-// var dino = require('./../js/Dino.js').Dino;
-// var fakeDinos = ["testasaurous", "fakeasaurous", "a", "b", "c"];
-//
-// $(document).ready(function(){
-//   var dinoArray = [];
-//
-//   $('#dino').submit(function(event) {
-//     event.preventDefault();
-//
-//     var dino1;
-//     var dino2;
-//     var dino3;
-//
-//     $.get(apiKey).then(function(response){
-//       var dino1 = response;
-//     });
-//     $.get(apiKey).then(function(response){
-//       var dino2 = response;
-//     });
-//     var randomPick = Math.floor(Math.random() * fakeDinos.length);
-//     var dino3 = (fakeDinos[randomPick]);
-//
-//     var newDino = new Dino(dino1, dino2, dino3);
-//     newDino.shuffle();
-//     dinoArray.push(newDino);
-//     console.log(dinoArray);
-//     dinoArray= [];
-//   });
-// });
--- js/dino.js
+++ js/dino.js
@@ -1,32 +1,32 @@
 var apiKey = require('./../.env').apiKey;
-var fakeDinos = ["testasaurous.", "fakeasaurous.", "a.", "b.", "c."];
+var fakeDinos = ["testasaurous", "fakeasaurous", "Gorosaurus", "Rhedosaurus", "Godzillasaurus", "Indominus Rex", "Titanosaurus", "Terrasaurus", "Dancrodon", "Quailadon"];
 var dinoArray = [];
 
 
 exports.DinoModule = function(){
 
 };
-exports.DinoModule.prototype.getDino = function(displayFunction) {
+exports.DinoModule.prototype.getDino = function(displayDino) {
   $.get(apiKey).then(function(response){
     var dinos = response;
     var dinoArray = [];
-    var splitDinos = dinos.split(" ");
+    var splitDinos = dinos.split(/[ .]/) ;
     dinoArray.push(splitDinos[0]);
     dinoArray.push(splitDinos[1]);
     var randomNumber = Math.floor(Math.random() * fakeDinos.length);
     dinoArray.push(fakeDinos[randomNumber]);
-    displayFunction(dinoArray);
+    displayDino(dinoArray);
   });
 
 };
 
 
 exports.DinoModule.prototype.shuffle = function(dinoArray) {
-    var j, x, i;
-    for (i = dinoArray.length; i; i -= 1) {
-        j = Math.floor(Math.random() * i);
-        x = dinoArray[i - 1];
-        dinoArray[i - 1] = dinoArray[j];
-        dinoArray[j] = x;
-    }
+  var j, x, i;
+  for (i = dinoArray.length; i; i -= 1) {
+    j = Math.floor(Math.random() * i);
+    x = dinoArray[i - 1];
+    dinoArray[i - 1] = dinoArray[j];
+    dinoArray[j] = x;
+  }
 };
